10199b22a4f2c22a1aa9fa9be54784912e232001
marc.blumberg <marcaugustblumberg@gmail.com>
2020-09-21 14:30:43 -0700

    Added a placeholder message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'rsuite/dist/styles/rsuite-default.css';
+import { Navbar } from 'rsuite';
+import { Container, Header, Content, Footer } from 'rsuite';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Header>
+          <Navbar appearance='subtle'>
+            <Navbar.Header>
+              <a href="#" className="navbar-brand">
+                LillyBelle And Marc
+              </a>
+            </Navbar.Header>
+          </Navbar>
+        </Header>
+        <Content>
+          <div className="main-info">
+            <div className="main-info-title">The Wedding Of</div>
+            <div className="main-info-title">LillyBelle Deer and Marc Blumberg</div>
+            <div className="main-info-title">May 22, 2021 at 6pm</div>
+            <div className="main-info-title">Heringer Estates - Clarksburg, CA</div>
+            <div className="main-info-subtitle">Details Coming Soon...</div>
+          </div>
+        </Content>
+        <Footer>
+        </Footer>
+      </Container>
     </div>
   );
 }
