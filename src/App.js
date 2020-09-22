8f278bb3fd7c79e14da426404a38766001a1b692
marc.blumberg <marcaugustblumberg@gmail.com>
2020-09-22 10:31:55 -0700

    Added a background image and routing capability

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import './App.css';
 import 'rsuite/dist/styles/rsuite-default.css';
 import { Navbar } from 'rsuite';
 import { Container, Header, Content, Footer } from 'rsuite';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Header>
-          <Navbar appearance='subtle'>
-            <Navbar.Header>
-              <a href="#" className="navbar-brand">
-                LillyBelle And Marc
-              </a>
-            </Navbar.Header>
-          </Navbar>
-        </Header>
-        <Content>
-          <div className="main-info">
-            <div className="main-info-title">The Wedding Of</div>
-            <div className="main-info-title">LillyBelle Deer and Marc Blumberg</div>
-            <div className="main-info-title">May 22, 2021 at 6pm</div>
-            <div className="main-info-title">Heringer Estates - Clarksburg, CA</div>
-            <div className="main-info-subtitle">Details Coming Soon...</div>
-          </div>
-        </Content>
-        <Footer>
-        </Footer>
-      </Container>
+      <Router>
+        <Container>
+          <Header>
+            <Navbar appearance='subtle'>
+              <Navbar.Header>
+                <Link to="/" className="navbar-brand">LillyBelle And Marc</Link>
+              </Navbar.Header>
+            </Navbar>
+          </Header>
+          <Content>
+            <Switch>
+              <Route exact path="/">
+                <Home></Home>
+              </Route>
+            </Switch>
+          </Content>
+          <Footer>
+          </Footer>
+        </Container>
+      </Router>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+function Home() {
+  return (
+    <div className="main-info">
+        <div className="main-info-title">The Wedding Of</div>
+        <div className="main-info-title">LillyBelle Deer and Marc Blumberg</div>
+        <div className="main-info-title">May 22, 2021 at 6pm</div>
+        <div className="main-info-title">Heringer Estates - Clarksburg, CA</div>
+        <div className="main-info-subtitle">Details Coming Soon...</div>
+    </div>
+  );
+}
+ 
+export default Home;
