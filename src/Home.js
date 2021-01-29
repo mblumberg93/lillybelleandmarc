e91212cc2bebb0b8f878eb1e7dd709e6a945e77e
mblumberg93 <marcaugustblumberg@gmail.com>
2021-01-29 12:47:28 -0800

    Dumb typo

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ function Home() {
           <img className="image main-info-image" alt="" src="./home.jpg" />
           <div className="main-info-title">The Wedding Of</div>
           <div className="main-info-title">LillyBelle Deer and Marc Blumberg</div>
-          <div className="main-info-sub-title">October, 16 at 6pm</div>
+          <div className="main-info-sub-title">October 16, 2021 at 6pm</div>
           <div className="main-info-sub-title">Heringer Estates - Clarksburg, CA</div>
       </div>
     </div>
